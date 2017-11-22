0f0ecad445c4165bd151ef1b1cf7a2560f168c37
handav <hannahdavis0220@gmail.com>
2017-11-21 19:39:57 -0500

    updating lesson 7

--- 7-kmeans/README.md
+++ 7-kmeans/README.md
@@ -1,5 +1,5 @@
 ## Implement K-means Clustering with Python and Scikit-learn To Find Clusters of Data
 
-We’ll return to the iris dataset to see how to use k-means clustering, an unsupervised learning algorithm, to create appropriate categories for data.
+We’ll return to the iris dataset to see how to use k-means clustering, an unsupervised learning algorithm, to create  categories for data that doesn't have labels.
 
 More on K-means can be found at http://scikit-learn.org/stable/modules/clustering.html#k-means.
--- 7-kmeans/kmeans.py
+++ 7-kmeans/kmeans.py
@@ -1,30 +1,18 @@
-import matplotlib.pyplot as plt
 from sklearn import datasets
-from sklearn import metrics
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 iris = datasets.load_iris()
-print iris.feature_names
 
-#just taking two features here
 X = iris.data[:, 1:3]
-y = iris.target
 
 model = KMeans(n_clusters=3, random_state=0)
 model.fit(X)
 
-print model.labels_
-print model.cluster_centers_
-print model.inertia_
-
-predictions = model.predict(X)
-
 centroids = model.cluster_centers_
-plt.scatter(centroids[:, 0], centroids[:, 1],
-            marker='^', s=170, linewidths=3,
-            color='m', zorder=10)
 
-plt.scatter(X[:, 0], X[:, 1], c=predictions)
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='^', s=170, zorder=10, c='m')
+plt.scatter(X[:, 0], X[:, 1], c=model.labels_)
 plt.xlabel("Sepal width")
 plt.ylabel("Petal length")
 plt.show()
